6e6d8cc9ec944ea2cc261aef57f40e6886f479a7
Andrew Noblet <andrewbnoblet@gmail.com>
2025-07-24 11:29:39 -0400

    feat(business-card): add front and back sides with planet icon
    Introduce a side property to business-card for front and back rendering. Front side displays a planet icon, back side shows contact and service info. Update planet icon to use relative sizing and currentColor. Add both card sides to business-card.astro for demonstration. Improve styling for better layout and clarity.

--- src/components/business-card.ts
+++ src/components/business-card.ts
@@ -1,18 +1,20 @@
 import { Base } from '@components/base';
 import { planet } from '@icons/planet';
 import { css, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { phone } from '@icons/phone';
 import { mail } from '@icons/mail';
 import { language } from '@icons/language';
 
 @customElement('business-card')
 export class BusinessCard extends Base {
+  @property({ type: String }) accessor side = 'front';
+
   static styles = [
     ...super.styles,
     css`
       :host {
-        --icon-padding: 0.5rem;
+        --icon-padding: 0;
 
         background-color: var(--color-neutral-900);
         border: 1px solid var(--color-neutral-500);
@@ -26,8 +28,18 @@ export class BusinessCard extends Base {
         color: var(--color-amber-300);
       }
 
+      .front {
+        icon-component {
+          --icon-size: calc(75% * (1 / 1.618));
+        }
+      }
+
       .padding {
         padding: 0.75rem;
+      }
+
+      .small {
+        font-size: 0.8rem;
       }
 
       .sky-200 {
@@ -40,6 +52,7 @@ export class BusinessCard extends Base {
       }
 
       ul {
+        height: max-content;
         padding: 0.5rem;
       }
     `,
@@ -47,39 +60,49 @@ export class BusinessCard extends Base {
 
   render() {
     return html`
-      <div class="column flex padding width-100">
-        <h1 class="text-align-center">Andrew Noblet</h1>
-        <p class="text-align-center">
-          Empowering your online presence with design, hosting, and expert
-          guidance.
-        </p>
-        <div class="flex space-around">
-          <ul class="amber-300">
-            <li>Design</li>
-            <li>Hosting</li>
-            <li>Consultation</li>
-          </ul>
-          <ul class="amber-300">
-            <li>Mentorship</li>
-            <li>Tutoring</li>
-            <li>Development</li>
-          </ul>
-        </div>
-        <div class="gap flex space-around">
-          <span>
-            <icon-component>${phone}</icon-component>
-            (914) 417-9070
-          </span>
-          <span>
-            <icon-component>${mail}</icon-component>
-            andrewbnoblet@gmail.com
-          </span>
-        </div>
-        <span class="justify-content-center text-align-center">
-          <icon-component>${language}</icon-component>
-          andrewbnoblet.com
-        </span>
-      </div>
+      ${this.side === 'front'
+        ? html`
+            <div
+              class="align-items-center flex front justify-content-center width-100"
+            >
+              <icon-component class="width-100">${planet}</icon-component>
+            </div>
+          `
+        : html`
+            <div class="column flex padding width-100">
+              <h1 class="text-align-center">Andrew Noblet</h1>
+              <p class="text-align-center">
+                Empowering your online presence with design, hosting, and expert
+                guidance.
+              </p>
+              <div class="align-items-center flex flex-1 space-around">
+                <ul class="amber-300">
+                  <li>Design</li>
+                  <li>Hosting</li>
+                  <li>Consultation</li>
+                </ul>
+                <ul class="amber-300">
+                  <li>Mentorship</li>
+                  <li>Tutoring</li>
+                  <li>Development</li>
+                </ul>
+              </div>
+              <div class="flex small space-around">
+                <span class="flex gap-0-5">
+                  <icon-component>${phone}</icon-component>
+                  (914) 417-9070
+                </span>
+                <span class="flex gap-0-5">
+                  <icon-component>${mail}</icon-component>
+                  andrewbnoblet@gmail.com
+                </span>
+                <span class="flex gap-0-5">
+                  <icon-component>${language}</icon-component>
+                  andrewnoblet.com
+                </span>
+              </div>
+            </div>
+          `}
     `;
   }
 }
